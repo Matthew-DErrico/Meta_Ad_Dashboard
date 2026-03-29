26e45cb606343d7dbd38d4bf3791319b5f9f6b70
Garrett-McClay <garrettmcclay0@gmail.com>
2026-03-29 18:17:13 -0400

    Update snowflake connection to utilize public/private key pair

--- backend/backend API/settings.py
+++ backend/backend API/settings.py
@@ -1,12 +1,24 @@
 from dotenv import load_dotenv
 import os
+from cryptography.hazmat.primitives import serialization
+
+with open("rsa_key.p8", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+    )
+pkb = private_key.private_bytes(
+    encoding=serialization.Encoding.DER,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption(),
+)
 
 load_dotenv()
 
 SNOWFLAKE_CONFIG = {
     "user": os.getenv("SNOWFLAKE_USER"),
-    "password": os.getenv("SNOWFLAKE_PASSWORD"),
     "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+    "private_key": pkb,
     "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
     "database": os.getenv("SNOWFLAKE_DATABASE"),
     "schema": os.getenv("SNOWFLAKE_SCHEMA"),
