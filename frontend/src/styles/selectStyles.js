f0dfeae0c08f74b90b5b2fe239676808908e954c
Matthew Ruhland <ruhmatt1973@gmail.com>
2026-04-14 13:59:23 -0400

    font styles

--- frontend/src/styles/selectStyles.js
+++ frontend/src/styles/selectStyles.js
@@ -8,16 +8,19 @@ export const getFilterDropdownStyle = (isDark) => ({
     borderRadius: "6px",
     backgroundColor: isDark ? "#1f1d1d" : "#ffffff",
     color: isDark ? "#F1EFE8" : "#2C2C2A",
+
   }),
   menu: (base) => ({
     ...base,
     width: "200px",
+    fontFamily: "'DM Mono', monospace",
     border: isDark ? "1px solid #464141" : "1px solid #d1d5db",
     borderRadius: "6px",
     backgroundColor: isDark ? "#1f1d1d" : "#ffffff",
   }),
   option: (base, state) => ({
     ...base,
+    fontFamily: "'DM Mono', monospace",
     backgroundColor: state.isFocused
       ? isDark
         ? "#2a2a2a"
